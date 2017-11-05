bb9cb6cfcda8b7e2a956719ac9b353339796955e
Raphael Bulle <raphael.bulle@uni.lu>
2017-11-05 16:07:08 +0100

    Add the right factors of the inverses of number of samples in the estimator definition

--- MLMC_heat_eq.py
+++ MLMC_heat_eq.py
@@ -6,31 +6,26 @@ from MLMC_one_level import Level_solver
 from math import sqrt
 
 '''
-We will increase the number of time steps, the number of triangles at the edge
-of the unit square and the number of samples in a geometric way, using the
-following sequence : 2**l
-In other terms, at each level we increase the number of time steps by
-multiplying by 2 and the number of triangles in the mesh by multiplying by
-sqrt(2).
+At each level we increase the number of time steps and the number of triangles at the edge of the square by
+multiplying them by 2.
 In the same way we decrease the number of samples by dividing by 2 at each
-level
+level.
+We start with 2 time steps, 16 triangles at the edge and 2**10 samples.
 '''
 
 '''
 MLMC variables
 '''
-l = 0               #Level number
-num_level = 6       #Number of levels
-random_param = 0    #Init the random parameter ~ LogNormal
-tri_mesh = [16*2**k for k in range(num_level-1)]
-time_steps = [2*2**k for k in range(num_level-1)]
-num_samples = [2**(10-k) for k in range(num_level-1)]
+num_level = int(input("Enter the number of levels (max 10):"))       #Number of levels
+random_param = 0    #Init the random parameter
+tri_mesh = [16*2**k for k in range(num_level)]
+time_steps = [2*2**k for k in range(num_level)]
+num_samples = [2**(10-k) for k in range(num_level)]
 
 '''
 Init result variables
 '''
-MLMC_estimator = 0
-level_estimator = 0
+MLMC_estimator = 0      #Final estimator
 
 '''
 Parameters of the initial Gaussian
@@ -41,7 +36,7 @@ sig = 1             #Variance of the Gaussian
 '''
 Finite Elements stuff
 '''
-meshes = [UnitSquareMesh(num_tri_mesh,num_tri_mesh) for num_tri_mesh in tri_mesh]       #Create a vector of meshes with increasing refinement
+meshes = [UnitSquareMesh(num_tri_mesh, num_tri_mesh) for num_tri_mesh in tri_mesh]       #Create a vector of meshes with increasing refinement
 V = [FunctionSpace(mesh, 'P', 1) for mesh in meshes]
 
 '''
@@ -53,16 +48,15 @@ init_fct = Expression('(4/sig - pow(2*(x[0]-m1)/sig,2) - pow(2*(x[1]-m2)/sig,2))
 '''
 Computation of the other levels
 '''
-for l in range(num_level-1):
+for l in range(num_level):
 
-    random_param = np.random.lognormal(0,1/8,num_samples[l])
+    random_param = np.random.lognormal(0, 1/8, num_samples[l])
 
     for k in random_param:
         if l == 0:
-            level_estimator += Level_solver(V[l],k, time_steps[l], init_fct)
+            MLMC_estimator += (1/(num_samples[l]))*Level_solver(V[l], k, time_steps[l], init_fct)
         else:
-            level_estimator += Level_solver(V[l],k, time_steps[l], init_fct) - Level_solver(V[l-1],k, time_steps[l-1], init_fct)
-
-    MLMC_estimator += (1/num_samples[l])*level_estimator
+            MLMC_estimator += (1/(num_samples[l]))*(Level_solver(V[l], k, time_steps[l], init_fct) - Level_solver(V[l-1], k, time_steps[l-1],
+                                                                                                                  init_fct))
 
 print(MLMC_estimator)
