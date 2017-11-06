285e8ab17b481f28fd8214005f68332c85e7aa93
Raphael Bulle <raphael.bulle@uni.lu>
2017-11-06 14:47:07 +0100

    Put the def of Init_fct inside MLMC_one_level.py and remove it from the parameters of the function Level_solver. Always issues with the output.

--- MLMC_heat_eq.py
+++ MLMC_heat_eq.py
@@ -18,11 +18,11 @@ results = open("MLMC_results.txt", "a")     #Create a file.txt in "add" mode
 '''
 MLMC variables
 '''
-num_level = int(input("Enter the number of levels (max 10):"))       #Number of levels
+num_level = int(input("Enter the number of levels (max 10):"))
 random_param = 0    #Init the random parameter
-tri_mesh = [16*2**k for k in range(num_level)]
-time_steps = [2*2**k for k in range(num_level)]
-num_samples = [2**(10-k) for k in range(num_level)]
+tri_mesh = [8*2**k for k in range(num_level)]      #Start with a mesh of 8*8*2 triangles
+time_steps = [2*2**k for k in range(num_level)]     #Start with 2 time steps
+num_samples = [2**(10-k) for k in range(num_level)] #Start with 2**10 samples
 
 '''
 Init result variables
@@ -30,37 +30,26 @@ Init result variables
 MLMC_estimator = 0      #Final estimator
 
 '''
-Parameters of the initial Gaussian
-'''
-mean = [0.5, 0.5]   #Mean of the Gaussian
-sig = 1             #Variance of the Gaussian
-
-'''
 Finite Elements stuff
 '''
 meshes = [UnitSquareMesh(num_tri_mesh, num_tri_mesh) for num_tri_mesh in tri_mesh]       #Create a vector of meshes with increasing refinement
-V = [FunctionSpace(mesh, 'P', 1) for mesh in meshes]
+V = [FunctionSpace(mesh, 'P', 1) for mesh in meshes]        #Vector of corresponding discrete functions spaces
 
 '''
-Initial solution
-'''
-init_fct = Expression('(4/sig - pow(2*(x[0]-m1)/sig,2) - pow(2*(x[1]-m2)/sig,2))*exp(-(pow(x[0]-m1,2) + pow(x[1]-m2,2))/sig)', sig=sig, m1=mean[0], m2=mean[1], degree=1)
-
-'''
-Computation of the other levels
+Computation of MLMC estimator
 '''
 for l in range(num_level):
 
     random_param = np.random.lognormal(0, 1/8, num_samples[l])
 
     for k in random_param:
-        if l == 0:
-            MLMC_estimator += (1/(num_samples[l]))*Level_solver(V[l], k, time_steps[l], init_fct)
+        if l == 0:          #The level 0 is different from the next ones
+            MLMC_estimator += (1/(num_samples[l]))*Level_solver(V[l], k, time_steps[l])
         else:
-            MLMC_estimator += (1/(num_samples[l]))*(Level_solver(V[l], k, time_steps[l], init_fct) - Level_solver(V[l-1], k, time_steps[l-1],
-                                                                                                                  init_fct))
+            MLMC_estimator += (1/(num_samples[l]))*(Level_solver(V[l], k, time_steps[l]) - Level_solver(V[l-1], k, time_steps[l-1]))
+
 results.write(str(num_level))
-results.write('\t')
+results.write('\t \t')
 results.write(str(MLMC_estimator))
 results.write('\n')
 results.close()
--- MLMC_one_level.py
+++ MLMC_one_level.py
@@ -15,9 +15,18 @@ Return :
     Time mean of the integrals of the solutions on the all domain
 '''
 
-def Level_solver(V,random_param, num_time_steps, init_fct):
+def Level_solver(V,random_param, num_time_steps):
     time_steps_lenght = 1/(num_time_steps)
     integral = 0
+
+    '''
+    Initial solution
+    '''
+    mean = [0.5, 0.5]   #Mean of the Gaussian
+    sig = 1             #Variance of the Gaussian
+
+    init_fct = Expression('(4/sig - pow(2*(x[0]-m1)/sig,2) - pow(2*(x[1]-m2)/sig,2))*exp(-(pow(x[0]-m1,2) + pow(x[1]-m2,2))/sig)', sig=sig,
+                          m1=mean[0], m2=mean[1], degree=1)
 
     u = TrialFunction(V)
     v = TestFunction(V)
@@ -29,9 +38,9 @@ def Level_solver(V,random_param, num_time_steps, init_fct):
 
     a = (u*v + Constant(random_param*time_steps_lenght)*dot(grad(u),grad(v)))*dx
     L = init_fct*v*dx
+    u = Function(V)
 
     for k in range(num_time_steps-1):
-        u = Function(V)
         solve(a == L, u, bc)
         integral += assemble(u*dx)
         init_fct = u
